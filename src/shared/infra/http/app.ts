d7c7101ee2a9aab536a5f422c8931e4739a20c39
Bruno Domingues <brunood07@gmail.com>
2021-11-22 11:46:49 -0300

    rate limiter e sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,18 +3,35 @@ import "dotenv/config";
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-error";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import swaggerUi from "swagger-ui-express";
 
 import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
+import upload from "@config/upload";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
-import upload from "@config/upload";
+// import rateLimiter from "@shared/infra/http/middlewares/rateLimiter"
 
 createConnection();
 const app = express();
+
+// app.use(rateLimiter);
+
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
@@ -25,6 +42,8 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,12 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
  
-export default async (): Promise<Connection> => {
+export default async (host = "database_ignite"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      database: process.env.NODE_ENV === 'test' ? "rentx_test" : defaultOptions.database,
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database: process.env.NODE_ENV === 'test' ? "rentx_test" : defaultOptions.database
     })
   );
-};
+}
